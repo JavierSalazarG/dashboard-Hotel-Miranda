6056d444bd4352e6a5af915520898b635e87d163
Javi <javis9555@gm@gmail.com>
2023-11-19 12:52:21 +0100

    init projet

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+//pages
+import App from "./App";
+import ErrorPage from "./pages/error-page";
+//------------
+import "./index.css";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
