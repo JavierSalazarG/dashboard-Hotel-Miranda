a12631c41ea8122623b364a2bd634be946346f69
Javi <javis9555@gm@gmail.com>
2023-11-28 14:08:15 +0100

    finish add users

--- src/features/users/UsersSlice.js
+++ src/features/users/UsersSlice.js
@@ -8,23 +8,31 @@ export const UserSlice = createSlice({
     status: "idle",
     error: null,
   },
-  reducers: {},
+  reducers: {
+    addUsers: (state, action) => {
+      state.data = [action.payload, ...state.data];
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(getUsersListFromAPIThunk.fulfilled, (state, action) => {
         state.status = "fulfilled";
-        state.data = action.payload;
+        state.data = [...state.data, ...action.payload];
       })
       .addCase(getUsersListFromAPIThunk.rejected, (state, action) => {
         state.status = "rejected";
-        state.error = action.error.message;
+        state.error = action.error.message || "Error desconocido";
       })
       .addCase(getUsersListFromAPIThunk.pending, (state) => {
         state.status = "pending";
+      })
+      .addDefaultCase((state) => {
+        state.status = "idle";
       });
   },
 });
 
+export const { addUsers } = UserSlice.actions;
 export const getUsersData = (state) => state.user.data;
 export const getUsersStatus = (state) => state.user.status;
 export const getUsersError = (state) => state.user.error;
--- src/pages/NewEmployee/NewEployee.jsx
+++ src/pages/NewEmployee/NewEployee.jsx
@@ -1,4 +1,8 @@
 import { MainStyled } from "../stytedPages";
+import { useDispatch } from "react-redux";
+import { addUsers } from "../../features/users/UsersSlice.js";
+import nextId from "react-id-generator";
+import perfilImage from "../../../public/navbar/perfil.png";
 import {
   FormStyled,
   InputStyled,
@@ -8,20 +12,44 @@ import {
   SubmitStyled,
 } from "./newEmployee.js";
 export const NewEployee = () => {
+  const id = nextId();
+  const dispatch = useDispatch();
+  const getCurrentDate = () => {
+    const currentDate = new Date();
+    const day = currentDate.getDate();
+    const month = currentDate.getMonth() + 1;
+    const year = currentDate.getFullYear();
+    return `${day}/${month}/${year}`;
+  };
   const HandleSubmit = (e) => {
     e.preventDefault();
+    const formData = {
+      foto: perfilImage,
+      nombre: e.target[0].value,
+      id: id,
+      email: e.target[1].value,
+      start_date: getCurrentDate(),
+      description: e.target[3].value,
+      contact: e.target[2].value,
+      status: true,
+    };
+    console.log(formData);
+    dispatch(addUsers(formData));
   };
   return (
     <MainStyled>
       <FormStyled onSubmit={HandleSubmit}>
         <InputStyled type="text" placeholder="Name and Surname" />
-        <inInputStyledput type="email" placeholder="Email" />
+        <InputStyled type="email" placeholder="Email" />
         <InputStyled type="number" placeholder="phone number" />
         <SelectStyled>
           <option>...</option>
-          <option>Manager</option>
-          <option>Recepción </option>
-          <option> Servicio de Habitaciones</option>
+          <option value="Manager">Manager</option>
+          <option value="Recepción">Recepción </option>
+          <option value="Servicio de Habitaciones">
+            {" "}
+            Servicio de Habitaciones
+          </option>
         </SelectStyled>
         <TexterraStyled placeholder="Functions description"></TexterraStyled>
         <LabelStyled>
